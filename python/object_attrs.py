833cd253f48dc21c239363f991af4a365c340f1b
Ayun Park <iamparkayun@gmail.com>
2015-09-02 14:27:08 +0900

    Update object_attrs.py

--- python/object_attrs.py
+++ python/object_attrs.py
@@ -1,12 +1,7 @@
 from __future__ import print_function
 
 
-def run_attr(obj):
+def print_attrs(obj):
     for attr in dir(obj):
-        print('>', attr, ':', end=" ")
-        obj_attr = getattr(obj, attr)
-        if hasattr(obj_attr, '__call__'):
-            print('method')
-        else:
-            print(obj_attr)
+        print('>', attr, ':', getattr(obj, attr))
 
